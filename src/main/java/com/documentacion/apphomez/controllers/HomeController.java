3222c552c6ce34407bb78b5a4649697c19142520
andresveleze31 <142637448+andresveleze31@users.noreply.github.com>
2024-02-26 20:39:08 -0500

    K8S New

--- src/main/java/com/documentacion/apphomez/controllers/HomeController.java
+++ src/main/java/com/documentacion/apphomez/controllers/HomeController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/grupo15")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String index(Model model) {
         return "index";
     }
 
-    @GetMapping("/grupo15/home")
+    @GetMapping("/home")
     public String home(Model model) {
         return "index";
     }
